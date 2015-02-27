91f2dfb2cf2ed963ba6024f15f446c75f79aec21
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-27 17:03:54 +0100

    Use utc timestamp instead of date

--- test/date.js
+++ test/date.js
@@ -8,17 +8,15 @@ var chai = require('chai'),
 describe('date', function () {
     describe('#toISODay', function () {
         it('formats date into YYYY-MM-DDT00:00:00.000Z', function () {
-            expect(date.toISODay(moment("1973-01-13 0:00 +0000", "YYYY-MM-DD HH:mm Z"))).to.equal('1973-01-13T00:00:00.000Z');
-            expect(date.toISODay(moment("1973-01-13 0:01 +0000", "YYYY-MM-DD HH:mm Z"))).to.equal('1973-01-13T00:00:00.000Z');
-            expect(date.toISODay(moment("1973-01-13 0:30 +0000", "YYYY-MM-DD HH:mm Z"))).to.equal('1973-01-13T00:00:00.000Z');
-            expect(date.toISODay(moment("1973-01-13 0:59 +0000", "YYYY-MM-DD HH:mm Z"))).to.equal('1973-01-13T00:00:00.000Z');
-            expect(date.toISODay(moment("1973-01-13 18:30 +0000", "YYYY-MM-DD HH:mm Z"))).to.equal('1973-01-13T00:00:00.000Z');
-            expect(date.toISODay(moment("1973-01-13 23:59 +0000", "YYYY-MM-DD HH:mm Z"))).to.equal('1973-01-13T00:00:00.000Z');
+            expect(date.toISODay(Date.UTC(1973, 0, 13, 0, 0, 0))).to.equal('1973-01-13T00:00:00.000Z');
+            expect(date.toISODay(Date.UTC(1973, 0, 13, 0, 1, 0))).to.equal('1973-01-13T00:00:00.000Z');
+            expect(date.toISODay(Date.UTC(1973, 0, 13, 18, 30, 0))).to.equal('1973-01-13T00:00:00.000Z');
+            expect(date.toISODay(Date.UTC(1973, 0, 13, 23, 59, 0))).to.equal('1973-01-13T00:00:00.000Z');
         });
     });
     describe('#toISOHour', function () {
         it('formats date into YYYY-MM-DDTHH:00:00.000Z', function () {
-            expect(date.toISOHour(moment("1973-01-13 0:00 +0000", "YYYY-MM-DD HH:mm Z"))).to.equal('1973-01-13T00:00:00.000Z');
+            expect(date.toISOHour(Date.UTC(1973, 0, 13, 0, 0, 0))).to.equal('1973-01-13T00:00:00.000Z');
         });
     });
 });
