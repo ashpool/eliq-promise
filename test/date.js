c320285b06c2e6a07ce912ce280f5c40df26655b
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-27 16:55:33 +0100

    Fix date stuff

--- test/date.js
+++ test/date.js
@@ -2,7 +2,7 @@
 var chai = require('chai'),
     expect = chai.expect,
     date = require('../lib/date'),
-    moment = require('moment');
+    moment = require('moment-timezone');
 
 
 describe('date', function () {
@@ -13,7 +13,7 @@ describe('date', function () {
             expect(date.toISODay(moment("1973-01-13 0:30 +0000", "YYYY-MM-DD HH:mm Z"))).to.equal('1973-01-13T00:00:00.000Z');
             expect(date.toISODay(moment("1973-01-13 0:59 +0000", "YYYY-MM-DD HH:mm Z"))).to.equal('1973-01-13T00:00:00.000Z');
             expect(date.toISODay(moment("1973-01-13 18:30 +0000", "YYYY-MM-DD HH:mm Z"))).to.equal('1973-01-13T00:00:00.000Z');
-            expect(date.toISODay(moment("1973-01-13 22:59 +0000", "YYYY-MM-DD HH:mm Z"))).to.equal('1973-01-13T00:00:00.000Z');
+            expect(date.toISODay(moment("1973-01-13 23:59 +0000", "YYYY-MM-DD HH:mm Z"))).to.equal('1973-01-13T00:00:00.000Z');
         });
     });
     describe('#toISOHour', function () {
--- test/eliqurl.js
+++ test/eliqurl.js
@@ -1,5 +1,6 @@
 /*jshint undef:false */
-var chai = require('chai'),
+var moment = require('moment-timezone'),
+    chai = require('chai'),
     expect = chai.expect;
 
 describe('eliqurl', function () {
@@ -7,7 +8,7 @@ describe('eliqurl', function () {
         it('reads config.eliqUrl and accessToken', function () {
             var config = {eliqUrl: 'https://eliq.url', eliqAccesstoken: 'xxxxx'},
                 eliqurl = require('./../lib/eliqurl')(config),
-                date = new Date(Date.UTC(1973, 0, 13, 0, 0, 0));
+                date = Date.UTC(1973, 0, 13, 1, 0);
             expect(eliqurl.day(date)).to.equal('https://eliq.url/?accesstoken=xxxxx&startdate=1973-01-13T00:00:00.000Z&intervaltype=hour');
         });
     });
