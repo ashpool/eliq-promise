8c0f69304a865a08e27b28373926e5d34c38adf6
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-27 19:03:04 +0100

    Add jshintrc

--- test/date.js
+++ test/date.js
@@ -8,15 +8,22 @@ var chai = require('chai'),
 describe('date', function () {
     describe('#toISODay', function () {
         it('formats date into YYYY-MM-DDT00:00:00.000Z', function () {
-            expect(date.toISODay(moment.tz("2012-11-04 00:59:59", "GMT"))).to.equal('2012-11-04T00:00:00.000Z');
+            expect(date.toISODay(moment.tz('2012-11-04 00:00:00', 'GMT'))).to.equal('2012-11-04T00:00:00.000Z');
+            expect(date.toISODay(moment.tz('2012-11-04 17:30:30', 'GMT'))).to.equal('2012-11-04T00:00:00.000Z');
+            expect(date.toISODay(moment.tz('2012-11-04 23:59:59', 'GMT'))).to.equal('2012-11-04T00:00:00.000Z');
 
-            expect(date.toISODay(moment.tz("2012-11-04 00:59:59", "America/New_York"))).to.equal('2012-11-04T04:00:00.000Z');
-            expect(date.toISODay(moment.tz("2012-11-04 00:59:59", "Europe/Stockholm"))).to.equal('2012-11-03T23:00:00.000Z');
+            expect(date.toISODay(moment.tz('2012-11-04 00:59:59', 'America/New_York'))).to.equal('2012-11-04T04:00:00.000Z');
+            expect(date.toISODay(moment.tz('2012-11-04 00:59:59', 'Europe/Stockholm'))).to.equal('2012-11-03T23:00:00.000Z');
         });
     });
     describe('#toISOHour', function () {
         it('formats date into YYYY-MM-DDTHH:00:00.000Z', function () {
-            expect(date.toISOHour(moment.tz("2012-11-04 17:30:15", "Europe/Stockholm"))).to.equal('2012-11-04T16:00:00.000Z');
+            expect(date.toISOHour(moment.tz('2012-11-04 00:00:00', 'GMT'))).to.equal('2012-11-04T00:00:00.000Z');
+            expect(date.toISOHour(moment.tz('2012-11-04 17:30:30', 'GMT'))).to.equal('2012-11-04T17:00:00.000Z');
+            expect(date.toISOHour(moment.tz('2012-11-04 23:59:59', 'GMT'))).to.equal('2012-11-04T23:00:00.000Z');
+
+            expect(date.toISOHour(moment.tz('2012-11-04 17:30:15', 'America/New_York'))).to.equal('2012-11-04T22:00:00.000Z');
+            expect(date.toISOHour(moment.tz('2012-11-04 17:30:15', 'Europe/Stockholm'))).to.equal('2012-11-04T16:00:00.000Z');
         });
     });
 });
--- test/eliqurl.js
+++ test/eliqurl.js
@@ -8,8 +8,16 @@ describe('eliqurl', function () {
         it('reads config.eliqUrl and accessToken', function () {
             var config = {eliqUrl: 'https://eliq.url', eliqAccesstoken: 'xxxxx'},
                 eliqurl = require('./../lib/eliqurl')(config),
-                date = moment.tz("2012-11-04 17:30:15", "GMT");
+                date = moment.tz('2012-11-04 17:30:15', 'GMT');
             expect(eliqurl.day(date)).to.equal('https://eliq.url/?accesstoken=xxxxx&startdate=2012-11-04T00:00:00.000Z&intervaltype=hour');
+        });
+    });
+    describe('#hour', function () {
+        it('reads config.eliqUrl and accessToken', function () {
+            var config = {eliqUrl: 'https://eliq.url', eliqAccesstoken: 'xxxxx'},
+                eliqurl = require('./../lib/eliqurl')(config),
+                date = moment.tz('2012-11-04 17:30:15', 'GMT');
+            expect(eliqurl.hour(date)).to.equal('https://eliq.url/?accesstoken=xxxxx&startdate=2012-11-04T17:00:00.000Z&intervaltype=6min');
         });
     });
 });
