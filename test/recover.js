b83e7983b0eef3be196314bdeb0757664d546ec7
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-03-17 07:48:30 +0100

    To see if Travis can run unit tests that touches the filesystem

--- test/recover.js
+++ test/recover.js
@@ -1,16 +1,21 @@
 /*jshint undef:false */
 /*jshint unused:false */
-var nock = require('nock'),
+var fs  = require("fs"),
+    nock = require('nock'),
     chai = require('chai'),
     expect = chai.expect;
 
 describe('recover', function () {
-    var recover = require('../lib/recover')({file: '/tmp/eliq.recover'});
+    var file = '/tmp/test-eliq.recover',
+        recover = require('../lib/recover')({file: file});
     describe('#log', function () {
-        it('writes url to file', function(done) {
-            var url = 'https://fuu.bar.com';
-            recover.log(url);
+        it('writes url to file', function (done) {
+            fs.closeSync(fs.openSync(file, 'w'));
+            var array = fs.readFileSync(file).toString();
+            expect(array).to.equal('');
 
+            //var url = 'https://fuu.bar.com';
+            //recover.log(url);
 
 
             done();
