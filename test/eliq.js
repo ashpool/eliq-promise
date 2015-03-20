a7727498762a6c211c205f897811020c638d9e0a
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-03-20 18:09:57 +0100

    Refactor tests

--- test/eliq.js
+++ test/eliq.js
@@ -22,47 +22,31 @@ describe('eliq', function () {
         });
         describe('#getFromTo', function () {
             it('fetches avgpower and energy', function (done) {
-                var scope = nock('https://my.eliq.se')
+                var result = {
+                    startdate: '2015-03-02T20:00:00+00:00',
+                    enddate: '2015-03-02T23:00:00+00:00',
+                    intervaltype: '6min',
+                    data: [{
+                        avgpower: 2790,
+                        energy: 279,
+                        temp_out: null,
+                        time_start: '2015-03-02T20:00:00',
+                        time_end: '2015-03-02T20:06:00'
+                    }]
+                };
+                nock('https://my.eliq.se')
                     .get('/api/data?accesstoken=fake&startdate=2015-03-02T20:00:00.000Z&enddate=2015-03-02T23:00:00.000Z&intervaltype=6min')
-                    .reply(200, {
-                        startdate: '2015-03-02T20:00:00+00:00',
-                        enddate: '2015-03-02T23:00:00+00:00',
-                        intervaltype: '6min',
-                        data: [{
-                            avgpower: 2790,
-                            energy: 279,
-                            temp_out: null,
-                            time_start: '2015-03-02T20:00:00',
-                            time_end: '2015-03-02T20:06:00'
-                        }]
-                    });
+                    .reply(200, result);
                 var eliq = require('../lib/eliq')({eliqAccesstoken: 'fake'});
-                eliq.getFromTo(new Date('2015-03-02T20:00:00+00:00'), new Date('2015-03-02T23:00:00+00:00'), '6min').then(function (result) {
-                    expect(result.startdate).to.equal('2015-03-02T20:00:00+00:00');
-                    expect(result.enddate).to.equal('2015-03-02T23:00:00+00:00');
-                    expect(result.intervaltype).to.equal('6min');
-                    scope.done();
-                    done();
-                }).catch(function (reason) {
-                    done(reason);
-                }).finally(function () {
-                    nock.cleanAll();
-                });
+                expect(eliq.getFromTo(new Date('2015-03-02T20:00:00+00:00'), new Date('2015-03-02T23:00:00+00:00'), '6min'))
+                    .to.eventually.deep.equal(result).notify(done);
             });
             it('throws an error when server is down', function (done) {
-                var scope = nock('https://my.eliq.se')
+                nock('https://my.eliq.se')
                     .get('/api/data?accesstoken=fake&startdate=2015-03-02T20:00:00.000Z&enddate=2015-03-02T23:00:00.000Z&intervaltype=6min')
                     .reply(503, {});
                 var eliq = require('../lib/eliq')({eliqAccesstoken: 'fake'});
-                eliq.getFromTo(new Date('2015-03-02T20:00:00+00:00'), new Date('2015-03-02T23:00:00+00:00'), '6min').then(function () {
-                    done('we should never get here');
-                }).catch(function (reason) {
-                    expect(reason).to.be.an.instanceof(Error);
-                    scope.done();
-                    done();
-                }).finally(function () {
-                    nock.cleanAll();
-                });
+                expect(eliq.getFromTo(new Date('2015-03-02T20:00:00+00:00'), new Date('2015-03-02T23:00:00+00:00'), '6min')).to.eventually.be.rejected.and.notify(done);
             });
         });
     });
