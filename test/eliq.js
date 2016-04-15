911de6a3e280cef7a563d07da0b5b7c8bc52392c
Magnus Ljadas <magnus.ljadas@gmail.com>
2016-04-15 21:18:54 +0200

    Introduce editorconfig
    Stolen from https://github.com/facebook/react/blob/master/.editorconfig

--- test/eliq.js
+++ test/eliq.js
@@ -1,53 +1,53 @@
 /*jshint undef:false */
 var nock = require('nock'),
-    chai = require('chai'),
-    chaiAsPromised = require('chai-as-promised'),
-    expect = chai.expect;
+  chai = require('chai'),
+  chaiAsPromised = require('chai-as-promised'),
+  expect = chai.expect;
 
 chai.use(chaiAsPromised);
 
 describe('eliq', function () {
-    describe('#getNow', function () {
+  describe('#getNow', function () {
 
-        afterEach(function () {
-            nock.cleanAll();
-        });
-
-        it('fetches createddate and power', function (done) {
-            nock('https://my.eliq.se')
-                .get('/api/datanow?accesstoken=fake')
-                .reply(200, {createddate: '2015-03-10T05:38:20', power: 1285});
-            var eliq = require('../lib/eliq')({eliqAccesstoken: 'fake'});
-            expect(eliq.getNow()).to.eventually.deep.equal({createddate: '2015-03-10T05:38:20', power: 1285}).notify(done);
-        });
-        describe('#getFromTo', function () {
-            it('fetches avgpower and energy', function (done) {
-                var result = {
-                    startdate: '2015-03-02T20:00:00+00:00',
-                    enddate: '2015-03-02T23:00:00+00:00',
-                    intervaltype: '6min',
-                    data: [{
-                        avgpower: 2790,
-                        energy: 279,
-                        temp_out: null,
-                        time_start: '2015-03-02T20:00:00',
-                        time_end: '2015-03-02T20:06:00'
-                    }]
-                };
-                nock('https://my.eliq.se')
-                    .get('/api/data?accesstoken=fake&startdate=2015-03-02T20:00:00.000&enddate=2015-03-02T23:00:00.000&intervaltype=6min')
-                    .reply(200, result);
-                var eliq = require('../lib/eliq')({eliqAccesstoken: 'fake'});
-                expect(eliq.getFromTo(new Date('2015-03-02T20:00:00+00:00'), new Date('2015-03-02T23:00:00+00:00'), '6min'))
-                    .to.eventually.deep.equal(result).notify(done);
-            });
-            it('throws an error when server is down', function (done) {
-                nock('https://my.eliq.se')
-                    .get('/api/data?accesstoken=fake&startdate=2015-03-02T20:00:00.000Z&enddate=2015-03-02T23:00:00.000Z&intervaltype=6min')
-                    .reply(503, {});
-                var eliq = require('../lib/eliq')({eliqAccesstoken: 'fake'});
-                expect(eliq.getFromTo(new Date('2015-03-02T20:00:00+00:00'), new Date('2015-03-02T23:00:00+00:00'), '6min')).to.eventually.be.rejected.and.notify(done);
-            });
-        });
+    afterEach(function () {
+      nock.cleanAll();
     });
+
+    it('fetches createddate and power', function (done) {
+      nock('https://my.eliq.se')
+        .get('/api/datanow?accesstoken=fake')
+        .reply(200, {createddate: '2015-03-10T05:38:20', power: 1285});
+      var eliq = require('../lib/eliq')({eliqAccesstoken: 'fake'});
+      expect(eliq.getNow()).to.eventually.deep.equal({createddate: '2015-03-10T05:38:20', power: 1285}).notify(done);
+    });
+    describe('#getFromTo', function () {
+      it('fetches avgpower and energy', function (done) {
+        var result = {
+          startdate: '2015-03-02T20:00:00+00:00',
+          enddate: '2015-03-02T23:00:00+00:00',
+          intervaltype: '6min',
+          data: [{
+            avgpower: 2790,
+            energy: 279,
+            temp_out: null,
+            time_start: '2015-03-02T20:00:00',
+            time_end: '2015-03-02T20:06:00'
+          }]
+        };
+        nock('https://my.eliq.se')
+          .get('/api/data?accesstoken=fake&startdate=2015-03-02T20:00:00.000&enddate=2015-03-02T23:00:00.000&intervaltype=6min')
+          .reply(200, result);
+        var eliq = require('../lib/eliq')({eliqAccesstoken: 'fake'});
+        expect(eliq.getFromTo(new Date('2015-03-02T20:00:00+00:00'), new Date('2015-03-02T23:00:00+00:00'), '6min'))
+          .to.eventually.deep.equal(result).notify(done);
+      });
+      it('throws an error when server is down', function (done) {
+        nock('https://my.eliq.se')
+          .get('/api/data?accesstoken=fake&startdate=2015-03-02T20:00:00.000Z&enddate=2015-03-02T23:00:00.000Z&intervaltype=6min')
+          .reply(503, {});
+        var eliq = require('../lib/eliq')({eliqAccesstoken: 'fake'});
+        expect(eliq.getFromTo(new Date('2015-03-02T20:00:00+00:00'), new Date('2015-03-02T23:00:00+00:00'), '6min')).to.eventually.be.rejected.and.notify(done);
+      });
+    });
+  });
 });
--- test/eliqurl.js
+++ test/eliqurl.js
@@ -1,45 +1,45 @@
 /*jshint undef:false */
 var moment = require('moment-timezone'),
-    chai = require('chai'),
-    expect = chai.expect;
+  chai = require('chai'),
+  expect = chai.expect;
 
 describe('eliqurl', function () {
-    it('throws an error if accesstoken is not configured', function () {
-        var eliqurl = require('./../lib/eliqurl');
-        expect(eliqurl).to.throw(Error);
-    });
+  it('throws an error if accesstoken is not configured', function () {
+    var eliqurl = require('./../lib/eliqurl');
+    expect(eliqurl).to.throw(Error);
+  });
 
-    describe('#now', function () {
-        it('reads config.Url and accessToken', function () {
-            var config = {eliqUrl: 'https://eliq.url', eliqAccesstoken: 'xxxxx'},
-                eliqurl = require('./../lib/eliqurl')(config);
-            expect(eliqurl.now()).to.equal('https://eliq.url/datanow?accesstoken=xxxxx');
-        });
-        it('has a default url', function () {
-            var config = {eliqAccesstoken: 'xxxxx'},
-                eliqurl = require('./../lib/eliqurl')(config);
-            expect(eliqurl.now()).to.equal('https://my.eliq.se/api/datanow?accesstoken=xxxxx');
-        });
+  describe('#now', function () {
+    it('reads config.Url and accessToken', function () {
+      var config = {eliqUrl: 'https://eliq.url', eliqAccesstoken: 'xxxxx'},
+        eliqurl = require('./../lib/eliqurl')(config);
+      expect(eliqurl.now()).to.equal('https://eliq.url/datanow?accesstoken=xxxxx');
     });
+    it('has a default url', function () {
+      var config = {eliqAccesstoken: 'xxxxx'},
+        eliqurl = require('./../lib/eliqurl')(config);
+      expect(eliqurl.now()).to.equal('https://my.eliq.se/api/datanow?accesstoken=xxxxx');
+    });
+  });
 
-    describe('#from', function () {
-        it('reads config.eliqUrl and accessToken', function () {
-            var config = {eliqUrl: 'https://eliq.url', eliqAccesstoken: 'xxxxx'},
-                eliqurl = require('./../lib/eliqurl')(config),
-                startdate = moment.tz('2012-11-04 17:30:15', 'GMT');
-            expect(eliqurl.from(startdate, '6min')).to
-                .equal('https://eliq.url/data?accesstoken=xxxxx&startdate=2012-11-04T17:00:00.000&intervaltype=6min');
-        });
+  describe('#from', function () {
+    it('reads config.eliqUrl and accessToken', function () {
+      var config = {eliqUrl: 'https://eliq.url', eliqAccesstoken: 'xxxxx'},
+        eliqurl = require('./../lib/eliqurl')(config),
+        startdate = moment.tz('2012-11-04 17:30:15', 'GMT');
+      expect(eliqurl.from(startdate, '6min')).to
+        .equal('https://eliq.url/data?accesstoken=xxxxx&startdate=2012-11-04T17:00:00.000&intervaltype=6min');
     });
+  });
 
-    describe('#fromTo', function () {
-        it('reads config.eliqUrl and accessToken', function () {
-            var config = {eliqUrl: 'https://eliq.url', eliqAccesstoken: 'xxxxx'},
-                eliqurl = require('./../lib/eliqurl')(config),
-                startdate = moment.tz('2012-11-03 17:30:15', 'GMT'),
-                enddate = moment.tz('2012-11-04 17:30:15', 'GMT');
-            expect(eliqurl.fromTo(startdate, enddate, 'day')).to
-                .equal('https://eliq.url/data?accesstoken=xxxxx&startdate=2012-11-03T17:00:00.000&enddate=2012-11-04T17:00:00.000&intervaltype=day');
-        });
+  describe('#fromTo', function () {
+    it('reads config.eliqUrl and accessToken', function () {
+      var config = {eliqUrl: 'https://eliq.url', eliqAccesstoken: 'xxxxx'},
+        eliqurl = require('./../lib/eliqurl')(config),
+        startdate = moment.tz('2012-11-03 17:30:15', 'GMT'),
+        enddate = moment.tz('2012-11-04 17:30:15', 'GMT');
+      expect(eliqurl.fromTo(startdate, enddate, 'day')).to
+        .equal('https://eliq.url/data?accesstoken=xxxxx&startdate=2012-11-03T17:00:00.000&enddate=2012-11-04T17:00:00.000&intervaltype=day');
     });
+  });
 });
